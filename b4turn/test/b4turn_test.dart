2cb00a6c92b9d1dd92dc84051f1f5939273a4cfc
Yatindra Nath Singh <ynsingh69@gmail.com>
2024-03-08 16:21:43 +0530

    b4turn package added.

---
+++ b4turn/test/b4turn_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:b4turn/b4turn.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
