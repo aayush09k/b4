f3a20870124aefff74bf2ec6ff8b992714b16ea9
Inderpal Singh <singh.inderpal.86@gmail.com>
2024-04-19 16:59:22 +0530

    Node ID RSA to EC change, Node id test code change, Auth package functions created, Auth package function test code, Pubspec.yaml file changed for node id and b4auth package

--- nodeid/test/nodeid_test.dart
+++ nodeid/test/nodeid_test.dart
@@ -7,11 +7,55 @@ void main() {
   LocalNodeID localnd = LocalNodeID();
   dynamic pvt1 = localnd.pvtKey;
   dynamic hash1 = localnd.nodeid.hashID;
+  print('hash1: $hash1');
   dynamic pub1 = localnd.nodeid.pubKey;
   dynamic sign1 = localnd.nodeid.sign;
+  print('sign1: $sign1');
+  dynamic publicKeyPem = localnd.nodeid.publicKeyPem;
+
+  //final publicKeyPem = CryptoUtils.encodeEcPublicKeyToPem(pub1);
+  Uint8List pubBytes2 = CryptoUtils.getBytesFromPEMString(publicKeyPem);
+  dynamic hash2 = CryptoUtils.getHash(pubBytes2, algorithmName: 'SHA-1');
+  print('hash2: $hash2');
+  test('hashID', () {
+    expect(hash1, hash2);
+  });
+
+  final List<int> codeUnits = (hash2 + publicKeyPem).codeUnits;
+  final Uint8List unit8List = Uint8List.fromList(codeUnits);
+  final sign2 = CryptoUtils.ecSign(pvt1, unit8List);
+  print('sign2: $sign2');
+  /*test('signature', () {
+    expect(sign1, sign2);
+  });*/
+
+  bool verify1 = CryptoUtils.ecVerify(pub1, unit8List, sign1);
+  print('verify1: $verify1');
+  bool verify2 = CryptoUtils.ecVerify(pub1, unit8List,sign2);
+  print('verify2: $verify2');
+  test('sign verify', () {
+    expect(verify1, verify2);
+  });
+
+  /*test('signature verification', () {
+    expect(verify, true);
+  });*/
+}
+
+
+
+/*void main() {
+  LocalNodeID localnd = LocalNodeID();
+  dynamic pvt1 = localnd.pvtKey;
+  dynamic hash1 = localnd.nodeid.hashID;
+  print('hash1: $hash1');
+  dynamic pub1 = localnd.nodeid.pubKey;
+  dynamic sign1 = localnd.nodeid.sign;
+  print('sign1: $sign1');
 
   Uint8List pubBytes = CryptoUtils.rsaPublicKeyModulusToBytes(pub1);
   dynamic hash2 = CryptoUtils.getHash(pubBytes, algorithmName: 'SHA-1');
+  print('hash2: $hash2');
   test('hashID', () {
     expect(hash1, hash2);
   });
@@ -20,6 +64,7 @@ void main() {
   final Uint8List unit8List = Uint8List.fromList(codeUnits);
   dynamic sign2 =
       CryptoUtils.rsaSign(pvt1, unit8List, algorithmName: 'SHA-256/RSA');
+  print('sign2: $sign2');
   test('signature', () {
     expect(sign1, sign2);
   });
@@ -28,4 +73,4 @@ void main() {
   test('signature verification', () {
     expect(verify, true);
   });
-}
+}*/
